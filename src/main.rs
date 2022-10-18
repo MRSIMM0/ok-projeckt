4eac124493dddcaac3158e7ac79af1809f9ddd52
MRSIMM0 <s.pasieczny02@gmail.com>
2022-10-18 12:51:00 +0200

    heuristic

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::io::BufReader;
-
+use std::cmp::Ordering;
 fn main(){
 
     let file_name = "data.txt";
@@ -14,22 +14,27 @@ fn main(){
   
     write_vector_to_file(file_name,&mut values);
     
-    let data = read_from_file(file_name);
+    let mut data = read_from_file(file_name);
+
+    heuristic(&mut data)
 
 }
 
 fn generate(n:&mut usize) -> Vec<(i32,i32)>{
 
     let mut arr=Vec::new();
+
     let mut rng = rand::thread_rng();
+
     for _x in 0..*n {
-        arr.push((rng.gen_range(0..2_000_000),rng.gen_range(0..2_000_000)));
+        arr.push((rng.gen_range(0..2_000),rng.gen_range(0..2_000)));
     }
 
    return arr;
 }
 
 fn write_vector_to_file(file_name: &str, arr: &mut Vec<(i32,i32)>){
+
     let path = Path::new(file_name);
     let display = path.display();
 
@@ -54,6 +59,7 @@ fn write_vector_to_file(file_name: &str, arr: &mut Vec<(i32,i32)>){
 }
 
 fn read_from_file(file_name: &str) -> Vec<(i32,i32)>{
+
     let path = Path::new(file_name);
     let display = path.display();
 
@@ -61,12 +67,15 @@ fn read_from_file(file_name: &str) -> Vec<(i32,i32)>{
         Err(why) => panic!("couldn't open {}: {}", display, why),
         Ok(file) => file ,
     };
+    
     let reader = BufReader::new(file);
 
     let mut result = Vec::new();
 
     for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); 
+
+        let line = line.unwrap();
+
         if index == 0{
             continue;
         }
@@ -76,6 +85,59 @@ fn read_from_file(file_name: &str) -> Vec<(i32,i32)>{
 
     }
 
-
     return result;
+}
+
+
+
+fn heuristic(arr: &mut Vec<(i32,i32)>){
+
+    let mut  visisted = Vec::<i32>::new();
+
+    recur(arr,&mut visisted,0);
+
+    println!("{:?}",visisted);
+}
+
+fn recur(arr: &mut Vec<(i32,i32)>, visisted: &mut Vec<i32>, current: i32){
+    visisted.push(current);
+
+    if arr.iter().len() == visisted.iter().len(){
+        return ;
+    }
+   
+    let distances = arr.iter().map(|x|   calculate_distance(arr[current as usize],*x) )
+    .collect::<Vec<f64>>();
+
+    let mut sorted = distances.clone();
+
+    sorted.sort_by(cmp_f64);
+
+    let mut min = 0;
+
+
+    while visisted.contains(&(sorted.iter()
+    .position(|&x| x == distances[min]).unwrap() as i32 )) {
+        min+=1;
+    }
+
+    let curr =sorted.iter()
+    .position(|&x| x == distances[min]).unwrap() as i32;
+
+    recur(arr,visisted,curr);
+}
+
+fn cmp_f64(a: &f64, b: &f64) -> Ordering {
+    if a < b {
+        return Ordering::Less;
+    } else if a > b {
+        return Ordering::Greater;
+    }
+    return Ordering::Equal;
+}
+
+fn calculate_distance(X1: (i32,i32), X2: (i32,i32)) -> f64 {
+    let  (x1,y1) = X1;
+    let (x2,y2) = X2;
+    return(((f64::powf((x2-x1).into(),2.0) + f64::powf((y2-y1).into(),2.0)) as f64 ).sqrt());
 }
