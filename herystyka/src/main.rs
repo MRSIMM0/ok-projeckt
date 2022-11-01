29b673a9ddf28a5642f01adc06fb1acdc2807c90
MRSIMM0 <s.pasieczny02@gmail.com>
2022-11-01 10:39:27 +0100

    project refactoring + simulated annealing

---
+++ herystyka/src/main.rs
@@ -0,0 +1,181 @@
+use rand::Rng;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::io::BufReader;
+use std::cmp::Ordering;
+use std::time::Instant;
+
+fn main(){
+
+    let file_name = "../data.txt";
+
+    let mut number:usize = 10;
+
+    let mut values = generate(&mut number);
+  
+    write_vector_to_file(file_name,&mut values);
+    
+    let mut data = read_from_file(file_name);
+
+
+    let output_file = "../path.txt";
+
+    let now = Instant::now();
+
+    let mut out = heuristic(&mut data);
+
+    let time = now.elapsed().as_secs_f64();
+
+    write_out_to_file(output_file, &mut out);
+
+
+    println!("{}", time);
+
+}
+
+fn generate(n:&mut usize) -> Vec<(i32,i32)>{
+
+    let mut arr=Vec::new();
+
+    let mut rng = rand::thread_rng();
+
+    for _x in 0..*n {
+        arr.push((rng.gen_range(0..2_000),rng.gen_range(0..2_000)));
+    }
+
+   return arr;
+}
+
+fn write_vector_to_file(file_name: &str, arr: &mut Vec<(i32,i32)>){
+
+    let path = Path::new(file_name);
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut result = format!("{}",arr.len());
+
+    for i in arr {
+        let (x,y) = i;
+        result = format!("{}\n{} {}",result,x,y);
+    }
+    file.write_all(result.as_bytes()).expect("write failed");
+
+    println!("data written to file" );
+
+}
+
+fn write_out_to_file(file_name: &str, arr: &mut Vec<i32>){
+    let path = Path::new(file_name);
+    let display = path.display();
+
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut result = format!("{}",arr.len());
+
+    for i in arr {
+        result = format!("{}\n{}",result,i);
+    }
+
+    file.write_all(result.as_bytes()).expect("write failed");
+
+    println!("out written to file" );
+
+}
+
+fn read_from_file(file_name: &str) -> Vec<(i32,i32)>{
+
+    let path = Path::new(file_name);
+    let display = path.display();
+
+    let file = match File::open(&path){
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file ,
+    };
+    
+    let reader = BufReader::new(file);
+
+    let mut result = Vec::new();
+
+    for (index, line) in reader.lines().enumerate() {
+
+        let line = line.unwrap();
+
+        if index == 0{
+            continue;
+        }
+
+        let x = line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        result.push((x[0],x[1]));
+
+    }
+
+    return result;
+}
+
+
+
+fn heuristic(arr: &mut Vec<(i32,i32)>) -> Vec::<i32>{
+
+    let mut  visisted = Vec::<i32>::new();
+
+    recur(arr,&mut visisted,0);
+
+    return visisted;
+}
+
+
+fn recur(arr: &mut Vec<(i32,i32)>, visisted: &mut Vec<i32>, current: i32){
+    visisted.push(current);
+
+    let current_element = arr[current as usize];
+
+
+    if arr.iter().len() == visisted.iter().len(){
+        return ;
+    }
+   
+    let distances = arr.iter()
+    .map(|x| calculate_distance(current_element,*x) )
+    .collect::<Vec<f64>>();
+
+    let mut sorted = distances.clone();
+
+    sorted.sort_by(cmp_f64);
+
+
+    let mut min = 0;
+
+
+    while visisted.contains(&(distances.iter()
+    .position(|&x| x == sorted[min]).unwrap() as i32) ) {
+        min+=1;
+    }
+
+    let curr =distances.iter()
+    .position(|&x| x == sorted[min]).unwrap() as i32;
+
+    recur(arr,visisted,curr);
+}
+
+fn cmp_f64(a: &f64, b: &f64) -> Ordering {
+    if a < b {
+        return Ordering::Less;
+    } else if a > b {
+        return Ordering::Greater;
+    }
+    return Ordering::Equal;
+}
+
+fn calculate_distance(x_1: (i32,i32), x_2: (i32,i32)) -> f64 {
+    let  (x1,y1) = x_1;
+    let (x2,y2) = x_2;
+    return(f64::powf((x2-x1).into(),2.0) + f64::powf((y2-y1).into(),2.0)).sqrt();
+}
---
+++ viz/script.js
@@ -0,0 +1,132 @@
+
+fetch("../data.txt")
+.then(response => response.text())
+.then(text => main(text))
+
+function main(data){
+
+
+    arr = data.split("\n")
+    arr.shift()
+ 
+    arr = arr.map((item)=>{
+        return item.split(" ");
+    });
+    
+    max_x = Math.max(...arr.map((item)=> item[0]));
+    min_x = Math.min(...arr.map((item)=> item[0]));
+
+    range_x = max_x - min_x;
+
+    max_y = Math.max(...arr.map((item)=> item[1]));
+    min_y = Math.min(...arr.map((item)=> item[1]));
+
+    range_y = max_y - min_y
+
+    normalized = arr.map((item)=> {
+        return [(item[0]-min_x)/range_x ,(item[1]-min_y)/range_y ]
+    } )
+
+    const canvas = document.querySelector('#canvas');
+
+    if (!canvas.getContext) {
+        return;
+    }
+
+    const ctx = canvas.getContext('2d');
+
+    var scale = 1;
+    canvas.width = 1280 * scale;
+    canvas.height = 739 * scale;
+
+    drawPoints(normalized,ctx);
+
+    fetch("../path.txt")
+    .then(response => response.text())
+    .then(path => {
+        arr = path.split("\n")
+        arr.shift()
+        arr.push(0)
+        drawLines(arr,ctx,"#1d8a3a");
+    })
+
+    fetch("../met-path.txt")
+    .then(response => response.text())
+    .then(path => {
+        arr = path.split("\n")
+        arr.shift()
+        arr.push(arr[0])
+        drawLines(arr,ctx,"red");
+    })
+
+}
+
+
+
+
+function drawLines(arr,ctx,color){
+
+    const main =  document.getElementById("main");
+
+    let calc_dist  = 0;
+
+    const distance = document.createElement("h1");
+
+
+
+    distance.style = "color:"+color
+
+    main.appendChild(distance)
+
+    for(let i =0; i<arr.length-1;i++){
+
+        let x_start = canvas.width*normalized[arr[i]][0];
+        let y_start = canvas.height*normalized[arr[i]][1];
+
+        let x_end = canvas.width*normalized[arr[i+1]][0];
+        let y_end = canvas.height*normalized[arr[i+1]][1];
+
+
+
+        ctx.beginPath();
+        ctx.moveTo(x_start, y_start);
+        ctx.lineTo(x_end, y_end);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        calc_dist+=get_distance(x_start,x_end,y_start,y_end);
+        
+        console.log(calc_dist)
+
+        distance.innerHTML = calc_dist;
+
+    }
+}
+
+
+function drawPoints(normalized,ctx){
+    normalized.forEach((x) =>{
+
+        let X = canvas.width*x[0];
+        let Y = canvas.height*x[1];
+        let R = 4;
+
+        
+
+        ctx.beginPath();
+        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#FF0000';
+        ctx.fillStyle = '#FF0000'
+        ctx.fill();
+        ctx.stroke();
+
+
+    })
+}
+
+function get_distance(x1,x2,y1,y2){
+return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2))
+}
+
+
+
